18fc367cd9862640b01321525eff4d629412a279
Arne Molland <arnemolland@Arnes-MacBook-Pro.local>
2020-04-27 19:28:49 +0200

    Updates scripts and workflow names

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd packages
-for D in *; do [ -d "${D}" ] && cd "${D}" && flutter analyze; done
+for D in *; do [ -d "${D}" ] && cd "${D}" && flutter analyze && cd ..; done
--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd packages
-for D in *; do [ -d "${D}" ] && cd "${D}" && flutter pub get; done
+for D in *; do [ -d "${D}" ] && cd "${D}" && flutter pub get && cd ..; done
