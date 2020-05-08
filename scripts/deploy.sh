2c1dcebcb36077cc1bb218993581cced61007ee9
Arne Molland <me@arnemolland.com>
2020-05-08 11:31:13 +0200

    Fix  temporary "dumb" deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,11 @@
 
 cd packages
 cd eufemia && pub publish -f
-cd eufemia_components && pub publish -f && cd ..
-cd eufemia_palette && pub publish -f && cd ..
-cd eufemia_spacing && pub publish -f && cd ..
-cd eufemia_typography && pub publish -f && cd ..
+cd .. && cd eufemia_components && pub publish -f
+cd .. && cd eufemia_palette && pub publish -f
+cd .. && cd eufemia_spacing && pub publish -f
+cd .. && cd eufemia_typography && pub publish -f
+
+exit 0
 
 #for D in *; do [ -d "${D}" ] && cd "${D}" && pub publish -f; done
